15aa0eb553b04ec8a330492b31f3683fe420bba2
Coloradete <48124115+Coloradete@users.noreply.github.com>
2022-07-01 15:11:39 +0200

    Added a little bit of comments in necessary sections

--- Assets/Scripts/HeroAbilities/AbilitiesHolder.cs
+++ Assets/Scripts/HeroAbilities/AbilitiesHolder.cs
@@ -27,39 +27,39 @@ namespace HeroAbilities
             TimeFalls
         }
 
-        private static GameObject[] abilitiesObjects;
+        private static GameObject[] loadedAbilitiesObjects;
         public static GameObject[] AbilitiesObjects; //Ordered by Enum
 
-        private static GameObject[] abilities;
+        private static GameObject[] loadedAbilities;
         public static GameObject[] Abilities; //Ordered by Enum
         static AbilitiesHolder()
         {
-            abilitiesObjects = Resources.LoadAll("AbilitiesObjects").Cast<GameObject>().ToArray();
+            loadedAbilitiesObjects = Resources.LoadAll("AbilitiesObjects").Cast<GameObject>().ToArray();
             AbilitiesObjects = new GameObject[System.Enum.GetValues(typeof(AbilityTypes)).Length];
 
-            abilities = Resources.LoadAll("Abilities").Cast<GameObject>().ToArray();
+            loadedAbilities = Resources.LoadAll("Abilities").Cast<GameObject>().ToArray();
             Abilities = new GameObject[System.Enum.GetValues(typeof(AbilityTypes)).Length];
 
             string abilityName;
 
-            for (int i = 0; i < abilitiesObjects.Length; i++)
+            for (int i = 0; i < loadedAbilitiesObjects.Length; i++)
             {
-                abilityName = abilitiesObjects[i].name;
-                abilityName = abilityName.Substring(0, abilityName.Length - 6);
+                abilityName = loadedAbilitiesObjects[i].name;
+                abilityName = abilityName.Substring(0, abilityName.Length - 6); //Remove the "Object" part
 
                 AbilityTypes abilityType = (AbilityTypes)System.Enum.Parse(typeof(AbilityTypes), abilityName, true);
 
-                AbilitiesObjects[(int)abilityType] = abilitiesObjects[i];
+                AbilitiesObjects[(int)abilityType] = loadedAbilitiesObjects[i]; //Re order the array by Enum
 
             }
-            for (int i = 0; i < abilities.Length; i++)
+            for (int i = 0; i < loadedAbilities.Length; i++)
             {
-                abilityName = abilities[i].name;
-                abilityName = abilityName.Substring(0, abilityName.Length - 7);
+                abilityName = loadedAbilities[i].name;
+                abilityName = abilityName.Substring(0, abilityName.Length - 7); //Remove the "Ability" part
 
                 AbilityTypes abilityType = (AbilityTypes)System.Enum.Parse(typeof(AbilityTypes), abilityName, true);
 
-                Abilities[(int)abilityType] = abilities[i];
+                Abilities[(int)abilityType] = loadedAbilities[i]; //Re order the array by Enum
             }
         }
     }
